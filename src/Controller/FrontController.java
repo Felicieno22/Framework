5273d585d66c572e871377b35ee45e464e98f02f
Felicieno22 <167999176+Felicieno22@users.noreply.github.com>
2024-05-24 18:49:02 +0000

    Update FrontController.java

--- src/Controller/FrontController.java
+++ src/Controller/FrontController.java
@@ -1,156 +1,120 @@
 package Controller;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
-<<<<<<< Updated upstream
-=======
-import java.lang.reflect.Method;
->>>>>>> Stashed changes
-import java.lang.reflect.Modifier;
-import java.net.URL;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.ArrayList;
-<<<<<<< Updated upstream
-import java.util.List;
-
-import AnnotationController.AnnotationController;
-=======
 import java.util.HashMap;
-import java.util.List;
+import java.lang.reflect.Method;
 
-import AnnotationController.AnnotationController;
-import AnnotationController.AnnotationGet;
-import classe.Mapping;
->>>>>>> Stashed changes
-import jakarta.servlet.ServletConfig;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import mg.itu.prom16.Annotations.*;
+import mg.itu.prom16.*;
 
 public class FrontController extends HttpServlet {
-<<<<<<< Updated upstream
-    boolean controllerScanee = false;
-    private String packageName; // Variable pour stocker le nom du package
-    private static List<String> controllerNames = new ArrayList<>();
+    private HashMap<String, Mapping> urlMappings = new HashMap<>();
+    protected ArrayList<String> listeControlleurs = new ArrayList<String>();
 
-=======
-    private String packageName; // Variable pour stocker le nom du package
-    private static List<String> controllerNames = new ArrayList<>();
-    HashMap <String,Mapping> urlMaping = new HashMap<>() ;
-    
->>>>>>> Stashed changes
-    @Override
-    public void init(ServletConfig config) throws ServletException {
-        super.init(config);
-        packageName = config.getInitParameter("packageControllerName"); // Récupération du nom du package
-<<<<<<< Updated upstream
-=======
-        scanControllers(packageName);
->>>>>>> Stashed changes
-    }
+    public void getListeControlleurs(String packagename) throws Exception {
+        String bin_path = "WEB-INF/classes/" + packagename.replace(".", "/");
+        bin_path = getServletContext().getRealPath(bin_path);
 
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        StringBuffer requestURL = request.getRequestURL();
-<<<<<<< Updated upstream
-        PrintWriter out = response.getWriter();
-
-        if (!controllerScanee) {
-            scanControllers(packageName, out);
-        }
-        response.setContentType("text/html");
-        out.println("<p>" + requestURL.toString() + "</p>");
-        out.close();
-    }
-
-=======
-        String[] requestUrlSplitted = requestURL.toString().split("/");
-        String controllerSearched = requestUrlSplitted[requestUrlSplitted.length-1];
-        
-        PrintWriter out = response.getWriter();
-        response.setContentType("text/html");
-
-        if (!urlMaping.containsKey(controllerSearched)) {
-            out.println("<p>"+"Aucune methode associee a ce chemin."+"</p>");
-        }
-        else {
-            Mapping mapping = urlMaping.get(controllerSearched);
-            
-            out.println("<p>" + requestURL.toString() + "</p>");
-            out.println("<p>" + mapping.getClassName() + "</p>");
-            out.println("<p>" + mapping.getMethodeName() + "</p>");
-
-            out.close();
-        }
-    }   
-
->>>>>>> Stashed changes
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-<<<<<<< Updated upstream
-    private void scanControllers(String packageName, PrintWriter out) {
-        try {
-=======
-    private void scanControllers(String packageName) {
-        try {
-
->>>>>>> Stashed changes
-            // Charger le package et parcourir les classes
-            ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-            String path = packageName.replace('.', '/');
-            URL resource = classLoader.getResource(path);
-            Path classPath = Paths.get(resource.toURI());
-            Files.walk(classPath)
-                    .filter(f -> f.toString().endsWith(".class"))
-                    .forEach(f -> {
-                        String className = packageName + "." + f.getFileName().toString().replace(".class", "");
-                        try {
-                            Class<?> clazz = Class.forName(className);
-                            if (clazz.isAnnotationPresent(AnnotationController.class)
-                                    && !Modifier.isAbstract(clazz.getModifiers())) {
-                                controllerNames.add(clazz.getSimpleName());
-<<<<<<< Updated upstream
-=======
-                                Method[] methods= clazz.getMethods();
-
-                                for(Method m : methods){
-                                    if(m.isAnnotationPresent(AnnotationGet.class)){
-                                        Mapping mapping =new Mapping(className , m.getName());
-                                        AnnotationGet AnnotationGet = m.getAnnotation(AnnotationGet.class);
-                                        String annotationValue = AnnotationGet.value();
-                                        urlMaping.put(annotationValue, mapping);
-                                    }
-                                }
->>>>>>> Stashed changes
-                            }
-                        } catch (ClassNotFoundException e) {
-                            e.printStackTrace();
+        File b = new File(bin_path);
+        for (File fichier : b.listFiles()) {
+            if (fichier.isFile() && fichier.getName().endsWith(".class")) {
+                String className = packagename + "." + fichier.getName().replace(".class", "");
+                Class<?> classe = Class.forName(className);
+                if (classe.isAnnotationPresent(Controller.class)) {
+                    for (Method method : classe.getDeclaredMethods()) {
+                        if (method.isAnnotationPresent(Get.class)) {
+                            Get getAnnotation = method.getAnnotation(Get.class);
+                            String url = getAnnotation.value();
+                            Mapping mapping = new Mapping(classe.getName(), method.getName());
+                            urlMappings.put(url, mapping);
                         }
-                    });
-<<<<<<< Updated upstream
-            for (String controller : controllerNames) {
-                out.println(controller);
+                    }
+                }
             }
-            controllerScanee = false;
-=======
->>>>>>> Stashed changes
+        }
+    }
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+        try {
+            getListeControlleurs(getServletContext().getInitParameter("controllerPackage"));
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    protected void processRequest(HttpServletRequest req, HttpServletResponse resp)
+    throws ServletException, IOException {
+        String contextPath = req.getContextPath(); // Obtenir le contexte de l'application
+        String urlPath = req.getRequestURI().substring(contextPath.length()); // Enlever le contexte de l'application        
+        if (urlPath == null || urlPath.isEmpty()) {
+            resp.getWriter().println("No URL path provided");
+            return;
+        }
+
+        Mapping mapping = urlMappings.get(urlPath);
+        PrintWriter out = resp.getWriter();
+        try {
+               // Afficher le contenu du HashMap urlMappings
+               out.println("Contenu de urlMappings:");
+               for (HashMap.Entry<String, Mapping> entry : urlMappings.entrySet()) {
+                   String url = entry.getKey();
+                   Mapping mapping2 = entry.getValue();
+                   out.println("URL: " + url + " - Class: " + mapping2.getClassName() + ", Method: " + mapping2.getMethodName());
+               }
+
+                getListeControlleurs(getServletContext().getInitParameter("controllerPackage"));
+                resp.setContentType("text/html;charset=UTF-8");
+                if (mapping != null) {
+                    out.println("<!DOCTYPE html>");
+                    out.println("<html>");
+                    out.println("<head>");
+                    out.println("<title>Mapping Information</title>");
+                    out.println("</head>");
+                    out.println("<body>");
+                    out.println("<h1>Information for URL: " + urlPath + "</h1>");
+                    out.println("<ul>");
+                    out.println("<li>Class Name: " + mapping.getClassName() + "</li>");
+                    out.println("<li>Method Name: " + mapping.getMethodName() + "</li>");
+                    out.println("</ul>");
+                    out.println("</body>");
+                    out.println("</html>");
+                } else {
+                    out.println("<!DOCTYPE html>");
+                    out.println("<html>");
+                    out.println("<head>");
+                    out.println("<title>No Mapping Found</title>");
+                    out.println("</head>");
+                    out.println("<body>");
+                    out.println("<h1>No method associated with this URL path: " + urlPath + "</h1>");
+                    out.println("</body>");
+                    out.println("</html>");
+                }            
+        } catch (Exception e) {
+            out.println(e.getMessage());
+            for (StackTraceElement ste : e.getStackTrace()) {
+                out.println(ste);
+            }
+        }
+    }
+
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        processRequest(req, resp);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        processRequest(req, resp);
     }
 
 }
