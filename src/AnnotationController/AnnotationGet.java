95a1b9550232668820c909f6909b19c80bcf084b
Felicieno22 <167999176+Felicieno22@users.noreply.github.com>
2024-05-24 18:46:42 +0000

    Update AnnotationGet.java

--- src/AnnotationController/AnnotationGet.java
+++ src/AnnotationController/AnnotationGet.java
@@ -1,9 +1,12 @@
 package AnnotationController;
 
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
-public @interface AnnotationGet {
+@Target(ElementType.METHOD)
+public @interface Get {
     String value();
 }
