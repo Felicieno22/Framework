c80cc7596eb71769733b41943f6f8acb8ec1e0a0
Felicieno22 <andriamandratofelicieno@gmail.com>
2024-05-24 18:42:10 +0000

    commit

---
+++ src/AnnotationController/AnnotationGet.java
@@ -0,0 +1,9 @@
+package AnnotationController;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface AnnotationGet {
+    String value();
+}
---
+++ src/getJar.bat
@@ -0,0 +1,8 @@
+@echo off
+    javac Controller/*.java
+<<<<<<< Updated upstream
+    jar cvf FrontController.jar Controller/*.class AnnotationController/*.class 
+=======
+    jar cvf FrontController.jar Controller/*.class AnnotationController/*.class classe/*.class 
+>>>>>>> Stashed changes
+pause
