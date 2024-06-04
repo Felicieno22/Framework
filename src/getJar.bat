8c7003b82a7bb87f74cddea8761fd0ae2303eb4a
Felicieno22 <167999176+Felicieno22@users.noreply.github.com>
2024-06-04 19:48:11 +0000

    Delete src/getJar.bat

--- src/getJar.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-    javac Controller/*.java
-<<<<<<< Updated upstream
-    jar cvf FrontController.jar Controller/*.class AnnotationController/*.class 
-=======
-    jar cvf FrontController.jar Controller/*.class AnnotationController/*.class classe/*.class 
->>>>>>> Stashed changes
-pause
