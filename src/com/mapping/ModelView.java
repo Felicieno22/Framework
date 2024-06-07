037db6530c5fc2f8d10bd76a076d61617c99ccd1
Felicieno22 <167999176+Felicieno22@users.noreply.github.com>
2024-06-07 23:33:13 +0200

    Update ModelView.java

--- src/com/mapping/ModelView.java
+++ src/com/mapping/ModelView.java
@@ -4,25 +4,28 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class ModelView {
-    private String url;
-    private Map<String, Object> data = new HashMap<>();
+    String url;                         //This is the url of the page 
+    HashMap<String,Object> properties;  //Attribut that  contains all the object that will be send to the page
 
-    public ModelView(String url) {
-        this.url = url;
+    public ModelView(){
+        properties = new HashMap<>();
+    }
+    //Method that will be used 
+    public void addObjet(String key,Object obj) {           //Method that used to add values to the controler
+        this.properties.put(key,obj);    
     }
 
-    public String getUrl() {
-        return url;
-    }
+    //Getter and setter 
     public void setUrl(String url) {
         this.url = url;	
     }
-    public Map<String, Object> getData() {
-        return data;
+    public String getUrl() {
+        return this.url;
     }
-    
-
-    public void addObject(String name, Object value) {
-        data.put(name, value);
+    public void setProperties(HashMap<String,Object> properties) {
+        this.properties = properties;
+    }
+    public HashMap<String,Object> getProperties() {
+        return this.properties;
     }
 }
