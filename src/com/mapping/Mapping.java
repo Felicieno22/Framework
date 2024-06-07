99db88df0bf2975777054a2fc92b7fad7df615d3
Felicieno22 <167999176+Felicieno22@users.noreply.github.com>
2024-06-07 23:25:51 +0200

    Update Mapping.java

--- src/com/mapping/Mapping.java
+++ src/com/mapping/Mapping.java
@@ -1,36 +1,20 @@
 package com.mapping;
 
 public class Mapping {
-    private String className;
-    private String methodName;
+    String nameClass;
+    String nameMethod; 
 
-    public Mapping(String className, String methodName) {
-        this.className = className;
-        this.methodName = methodName;
+    public void add(String n1, String n2) {
+        this.nameClass = n1;
+        this.nameMethod = n2;
     }
 
-    public String getClassName() {
-        return className;
+    public String getValue() {
+        return nameMethod;
     }
 
-    public void setClassName(String className) {
-        this.className = className;
+    public String getKey(){
+        return nameClass;
     }
-
-    public String getMethodName() {
-        return methodName;
-    }
-
-    public void setMethodName(String methodName) {
-        this.methodName = methodName;
-    }
-
-    @Override
-    public String toString() {
-        return "Mapping{" +
-                "className='" + className + '\'' +
-                ", methodName='" + methodName + '\'' +
-                '}';
-    }
-
 }
+
