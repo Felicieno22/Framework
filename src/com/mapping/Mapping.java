6cb333bc71895350921caed33d59248bd0ef09ab
Felicieno22 <167999176+Felicieno22@users.noreply.github.com>
2024-10-21 06:53:35 +0300

    commit to main

--- src/com/mapping/Mapping.java
+++ src/com/mapping/Mapping.java
@@ -1,8 +1,11 @@
 package com.mapping;
 
+import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
+
 public class Mapping {
     String nameClass;
-    String nameMethod; 
+    Method nameMethod; 
 
     public void add(String n1, String n2) {
         this.nameClass = n1;
@@ -15,6 +18,31 @@ public class Mapping {
 
     public String getKey(){
         return nameClass;
+    }
+    @Override
+    public String toString() {
+        return "Mapping{" +
+                "className='" + nameClass + '\'' +
+                ", methodName='" + nameMethod.getName() + '\'' +
+                '}';
+    }
+
+    public String method_to_string() {
+        StringBuilder methodString = new StringBuilder();
+        methodString.append(nameMethod.getName()).append("(");
+
+        Parameter[] parameters = nameMethod.getParameters();
+
+        
+        for (int i = 0; i < parameters.length; i++) {
+            methodString.append(parameters[i].getType().getSimpleName()+" "+parameters[i].getName());
+            if (i < parameters.length - 1) {
+                methodString.append(", ");
+            }
+        }
+
+        methodString.append(")");
+        return methodString.toString();
     }
 }
 
